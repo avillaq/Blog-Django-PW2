978c458d0984346940a29ed34329a3338be4d0eb
avillaq <villafuertequispealex@gmail.com>
2024-06-06 22:11:56 -0500

    Campo "updated" eliminado del modelo post

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,6 @@ class Post(models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2,
                               choices=Estado.choices,
                               default=Estado.BORRADOR)
