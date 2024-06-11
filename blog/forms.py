297c5f4c46de19d956073a96baaaf74552747aa1
avillaq <villafuertequispealex@gmail.com>
2024-06-11 17:56:06 -0500

    Adicion de comentarios

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class EmailPostForm(forms.Form):
     nombre = forms.CharField(max_length=25)
     email = forms.EmailField()
     para = forms.EmailField()
-    comentarios = forms.CharField(required=False, widget=forms.Textarea)
+    comentarios = forms.CharField(required=False, widget=forms.Textarea) # El campo comentarios es opcional.
     
 # Formulario basado en el modelo Comment
 class ComentarioForm(forms.ModelForm):
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+# Define el espacio de nombres de la aplicación. Sirve para diferenciar las URL de otras aplicaciones con el mismo nombre.
 app_name = 'blog' 
 urlpatterns = [
     path('', views.PostListaView.as_view(), name='post_lista'), # Esta es la vista basada en clases
